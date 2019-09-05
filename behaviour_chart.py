f8d2655fcd04e429a3361634106d5f2e293ded8c
socalNerdtastic <socal.nerdtastic@gmail.com>
2019-09-04 20:50:45 -0700

    replaced place with grid and pack

--- behaviour_chart.py
+++ behaviour_chart.py
@@ -2,13 +2,19 @@
 import tkinter as tk
 import datetime
 
-def add_happy_sticker(happy_x_pos, happy_y_pos):
+COLUMNS = 4 # number of columns of happy / sad faces
+
+def add_happy_sticker():
     """
     Displays sticker to screen
     as set by happy_button_call,
     writes details to log
     """
     global happy_count
+    happy = tk.Label(happy_faces, image=happy_img, bg="light green")
+    r, c = divmod(happy_count, COLUMNS)
+    happy.grid(row=r, column=c)
+
     now = datetime.datetime.now()
     timedate = now.strftime("%A, %B %d, %Y ")+now.strftime("%I:%M%p:")
     happy_count += 1
@@ -16,17 +22,19 @@ def add_happy_sticker(happy_x_pos, happy_y_pos):
     file = open("log/behaviour.log", "a")
     file.write(str(timedate)+"\nBehaviour: GOOD\nGood count: " \
                                         +str(happy_count)+"\n\n")
-    file.close() 
-    happy = tk.Label(happy_frame, image=happy_img, bg="light green")
-    happy.place(x=happy_x_pos, y=happy_y_pos)
+    file.close()
 
-def add_sad_sticker(sad_x_pos, sad_y_pos):
+def add_sad_sticker():
     """
     Displays sticker to screen
     as set by sad_button_call,
     writes details to log
     """
     global sad_count
+    sad = tk.Label(sad_faces, image=sad_img, bg="red3")
+    r, c = divmod(sad_count, COLUMNS)
+    sad.grid(row=r, column=c)
+
     now = datetime.datetime.now()
     timedate = now.strftime("%A, %B %d, %Y ")+now.strftime("%I:%M%p:")
     sad_count += 1
@@ -34,134 +42,53 @@ def add_sad_sticker(sad_x_pos, sad_y_pos):
     file = open("log/behaviour.log", "a")
     file.write(str(timedate)+"\nBehaviour: BAD\nBad count: " \
                                         +str(sad_count)+"\n\n")
-    file.close() 
-    sad = tk.Label(sad_frame, image=sad_img, bg="red3")
-    sad.place(x=sad_x_pos, y=sad_y_pos)
-    
-def happy_button_call():
-    """
-    On button press, this will 
-    set the position of the sticker
-    according to their x,y values
-    set by the conditionals below,
-    then calls the function
-    add_happy_sticker
-    """
-    global happy_x_pos, happy_y_pos
-    print(happy_x_pos)
-    
-    if happy_x_pos == 0:
-        add_happy_sticker(happy_x_pos, happy_y_pos)
-        happy_x_pos += 75
-    elif happy_x_pos == 75:
-        happy_x_pos += 1
-    if happy_x_pos == 76:
-        happy_x_pos = 75
-        add_happy_sticker(happy_x_pos, happy_y_pos)
-        happy_x_pos += 75
-    elif happy_x_pos > 75 and happy_x_pos != 76:
-        add_happy_sticker(happy_x_pos, happy_y_pos)
-        happy_x_pos += 75
-    
-    if happy_x_pos > 225:
-        happy_x_pos = 0
-        if happy_y_pos == 0:
-            add_happy_sticker(happy_x_pos, happy_y_pos)
-            happy_y_pos += 75
-        elif happy_y_pos == 75:
-            happy_y_pos += 1
-        if happy_y_pos == 76:
-            happy_y_pos = 75
-            add_happy_sticker(happy_x_pos, happy_y_pos)
-            happy_y_pos += 75
-        elif happy_y_pos > 75 and happy_y_pos != 76:
-            add_happy_sticker(happy_x_pos, happy_y_pos)
-            happy_y_pos += 75
-    return happy_x_pos, happy_y_pos
-
-def sad_button_call():
-    """
-    On button press, this will 
-    set the position of the sticker
-    according to their x,y values
-    set by the conditionals below,
-    then calls the function
-    add_sad_sticker 
-    """
-    global sad_x_pos, sad_y_pos
-    print(sad_x_pos)
-    
-    if sad_x_pos == 0:
-        add_sad_sticker(sad_x_pos, sad_y_pos)
-        sad_x_pos += 75
-    elif sad_x_pos == 75:
-        sad_x_pos +=1
-    if sad_x_pos == 76:
-        sad_x_pos = 75
-        add_sad_sticker(sad_x_pos, sad_y_pos)
-        sad_x_pos += 75
-    elif sad_x_pos > 75 and sad_x_pos != 76:
-        add_sad_sticker(sad_x_pos, sad_y_pos)
-        sad_x_pos += 75
-    
-    if sad_x_pos > 225:
-        sad_x_pos = 0
-        if sad_y_pos == 0:
-            add_sad_sticker(sad_x_pos, sad_y_pos)
-            sad_y_pos += 75
-        elif sad_y_pos == 75:
-            sad_y_pos +=1
-        if sad_y_pos == 76:
-            sad_y_pos = 75
-            add_sad_sticker(sad_x_pos, sad_y_pos)
-            sad_y_pos += 75
-        elif sad_y_pos > 75 and sad_y_pos != 76:
-            add_sad_sticker(sad_x_pos, sad_y_pos)
-            sad_y_pos += 75
-    return sad_x_pos, sad_y_pos
-    
+    file.close()
 
 root = tk.Tk()
-root.wm_attributes("-type", "splash")
+# ~ root.wm_attributes("-type", "splash")
 root.geometry("900x750")
 
-happy_x_pos = 0
-happy_y_pos = 0
 happy_count = 0
-sad_x_pos = 0
-sad_y_pos = 0
 sad_count = 0
 
 happy_img = tk.PhotoImage(file = "gfx/happy.png") # dimensions 75x75
 sad_img = tk.PhotoImage(file = "gfx/sad.png") # dimensions 75x75
 name_img = tk.PhotoImage(file = "gfx/name.png") # dimensions 200x237
-                                                
+
 happy_frame = tk.Frame(root, height=750, width=300, bg="light green")
-happy_frame.pack(side=tk.LEFT)
-sad_frame = tk.Frame(root, height=750, width=300, bg="red3")
-sad_frame.pack(side=tk.RIGHT)
+happy_frame.pack_propagate(False) # obey size definition above (do not adjust to child widget size)
+happy_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+happy_faces = tk.Frame(happy_frame, bg="light green")
+happy_faces.pack(fill=tk.BOTH, expand=True)
+# when using an image, let the size of the image define the Button size; don't override it
+happy_button = tk.Button(happy_frame, # note newline escapes are not needed
+    command=add_happy_sticker,
+    image=happy_img, bg="black")
+happy_button.pack(side=tk.BOTTOM)
+
+# set up the name Frame
 name_frame = tk.Frame(root, height=750, width=300,bg="cyan")
-name_frame.pack()
+name_frame.rowconfigure(1, weight=1) # fill all available vertical space, giving any extra room to the middle row
+name_frame.columnconfigure(0, weight=1) # fill all available horizontal space
+name_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
 name = tk.Label(name_frame, image=name_img, bg="cyan")
-name.place(x=50, y=0)                    
+name.grid(sticky=tk.N)
 
-name_label = tk.Label(name_frame, text="Name's", bg="cyan") # This 
-name_label.config(font=("MonospaceRegular, 38"))       # may need to be 
-name_label.place(x=61, y=300)              # adjusted according to name
+name_label = tk.Label(name_frame, text="Name's\nBehaviour", bg="cyan") # This
+name_label.config(font=("MonospaceRegular, 38"))       # may need to be
+name_label.grid()              # adjusted according to name
 
-behaviour_label = tk.Label(name_frame, text="Behaviour", bg="cyan")
-behaviour_label.config(font=("MonospaceRegular, 38"))
-behaviour_label.place(x=38, y=360)
 good_bad_label = tk.Label(name_frame, text="GOOD\nor\nBAD", bg="cyan")
 good_bad_label.config(font=("MonospaceRegular, 50"))
-good_bad_label.place(x=48, y=500)
+good_bad_label.grid(sticky=tk.S)
 
-happy_button = tk.Button(happy_frame, height=67, width=67, \
-                command=happy_button_call, image=happy_img, bg="black")
-happy_button.place(x=105, y=674)
-
-sad_button = tk.Button(sad_frame, height=67, width=67, \
-                    command=sad_button_call, image=sad_img, bg="black")
-sad_button.place(x=115, y=673)
+sad_frame = tk.Frame(root, height=750, width=300, bg="red3")
+sad_frame.pack_propagate(False)
+sad_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+sad_faces = tk.Frame(sad_frame, bg="red3")
+sad_faces.pack(fill=tk.BOTH, expand=True)
+sad_button = tk.Button(sad_frame, command=add_sad_sticker, image=sad_img, bg="black")
+sad_button.pack(side=tk.BOTTOM)
 
 root.mainloop()
