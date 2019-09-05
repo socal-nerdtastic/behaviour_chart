4c47d7f40f9a32b787b7c0e460d309ccde345ff3
socalNerdtastic <socal.nerdtastic@gmail.com>
2019-09-04 22:03:02 -0700

    move to Text widgets

--- behaviour_chart.py
+++ behaviour_chart.py
@@ -20,49 +20,51 @@ class StickerFrame(tk.Frame):
         self.img = tk.PhotoImage(file = f'gfx/{behaviour}.png')
         self.count = 0
 
-        self.stickers = tk.Frame(self, bg=self['bg'])
-        self.stickers.pack()
+        self.stickers = tk.Text(self, width=2, bg=self['bg'], highlightthickness=0, border=0)
+        self.stickers.pack(fill=tk.BOTH, expand=True)
         btn = tk.Button(self, command=self.add_sticker, image=self.img, bg="black")
         btn.pack(side=tk.BOTTOM)
 
     def add_sticker(self):
-        lbl = tk.Label(self.stickers, image=self.img, bg=self['bg'])
-        r, c = divmod(self.count, COLUMNS)
-        lbl.grid(row=r, column=c)
+        self.stickers.image_create(tk.END, image=self.img)
         self.count += 1
         add_to_log(self.behaviour, self.count)
 
+class NameFrame(tk.Frame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, bg="cyan", **kwargs)
+        self.rowconfigure(1, weight=1) # fill all available vertical space, giving any extra room to the middle row
+        self.columnconfigure(0, weight=1) # fill all available horizontal space
+
+        self.name_img = tk.PhotoImage(file = "gfx/name.png") # dimensions 200x237
+        name = tk.Label(self, image=self.name_img, bg="cyan")
+        name.grid(sticky=tk.N)
+
+        name_label = tk.Label(self, text="Name's\nBehaviour", bg="cyan") # This
+        name_label.config(font=("MonospaceRegular, 38"))       # may need to be
+        name_label.grid()              # adjusted according to name
+
+        good_bad_label = tk.Label(self, text="GOOD\nor\nBAD", bg="cyan")
+        good_bad_label.config(font=("MonospaceRegular, 50"))
+        good_bad_label.grid(sticky=tk.S)
+
 def main():
     root = tk.Tk()
-    root.wm_attributes("-type", "splash")
     root.geometry("900x750")
+    root.rowconfigure(0, weight=1) # fill all available vertical space
+    # fill all available horizontal space, giving any extra room equally to column index 0 and 2 (the side columns)
+    root.columnconfigure(0, weight=1)
+    root.columnconfigure(2, weight=1)
 
-    name_img = tk.PhotoImage(file = "gfx/name.png") # dimensions 200x237
-
-    happy_frame = StickerFrame(root, behaviour='good', height=750, width=300, bg="light green")
-    happy_frame.pack_propagate(False) # obey size definition above (do not adjust to child widget size)
-    happy_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+    happy_frame = StickerFrame(root, behaviour='good', bg="light green")
+    happy_frame.grid(row=0, column=0, sticky='nsew')
 
     # set up the name Frame
-    name_frame = tk.Frame(root, height=750, width=300,bg="cyan")
-    name_frame.rowconfigure(1, weight=1) # fill all available vertical space, giving any extra room to the middle row
-    name_frame.columnconfigure(0, weight=1) # fill all available horizontal space
-    name_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-
-    name = tk.Label(name_frame, image=name_img, bg="cyan")
-    name.grid(sticky=tk.N)
-
-    name_label = tk.Label(name_frame, text="Name's\nBehaviour", bg="cyan") # This
-    name_label.config(font=("MonospaceRegular, 38"))       # may need to be
-    name_label.grid()              # adjusted according to name
-
-    good_bad_label = tk.Label(name_frame, text="GOOD\nor\nBAD", bg="cyan")
-    good_bad_label.config(font=("MonospaceRegular, 50"))
-    good_bad_label.grid(sticky=tk.S)
+    name_frame = NameFrame(root)
+    name_frame.grid(row=0, column=1, sticky='nsew')
 
     sad_frame = StickerFrame(root, behaviour='bad', height=750, width=300, bg="red3")
-    sad_frame.pack_propagate(False)
-    sad_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+    sad_frame.grid(row=0, column=2, sticky='nsew')
 
     root.mainloop()
 
